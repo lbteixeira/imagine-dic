8471b0b19586cf9adeb06d03e05181fced1e8297
Lucas Teixeira <lucas@lbteixeira.com>
2022-03-08 17:47:56 +0100

    documentation for the InterpBLockNode class

--- src/imagine/math/coefficients.hpp
+++ src/imagine/math/coefficients.hpp
@@ -5,14 +5,39 @@
 
 namespace Imagine {
   namespace Math {
-
+    /**
+     *  @brief A class to represent interpolation block nodes.
+     *
+     *  The table of interpolation coefficients (\p CoefficientsTable) is
+     *  composed of interpolation block nodes, that contain a flag to indicate
+     *  if this block was already calculated and a vector with the
+     *  corresponding coefficients.
+     */
     class InterpBlockNode {
       public:
+        /**
+         * Constructor used to define an empty node. Used the initialization of
+         * a \p CoefficientsTable object.
+         */
         InterpBlockNode();
+        /**
+         * Constructor used to define a non-empty node.
+         * @param coefficients vector of coefficients used for the interpolation.
+         */
         explicit InterpBlockNode(const std::vector<double>& coefficients);
 
+        /**
+         * @brief Returns true if the vector of coefficients is empty.
+         */
         bool isEmpty() const;
+        /**
+         * @brief Returns the vector of coefficients.
+         */
         const std::vector<double>& getCoefficients() const;
+        /**
+         * @brief Sets the vector of coefficients.
+         * @param coefficients a vector of interpolation coefficients.
+         */
         void setCoefficients(const std::vector<double>& coefficients);
 
       private:
