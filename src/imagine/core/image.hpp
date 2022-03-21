72635b866088c8c4db6a427492b4dce1d70e8d76
Lucas Teixeira <lucas@lbteixeira.com>
2022-03-21 07:49:54 +0100

    new paramters to mesh

--- src/imagine/core/image.hpp
+++ src/imagine/core/image.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+#include "interpolation.hpp"
+#include "mesh.hpp"
+#include <memory>
+
 namespace Imagine {
   namespace Core {
     class Image {
       public:
         Image();
+
+      private:
+        std::unique_ptr<Mesh::Mesh> _mesh;
+        std::unique_ptr<Interpolator> _interpolator;
     };
   }
 }
