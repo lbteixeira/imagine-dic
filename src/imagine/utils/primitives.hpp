cec8b19fa8599daf0cb1676f4fbb0dbaf9a7df1a
Lucas Teixeira <lucas@lbteixeira.com>
2022-03-21 07:49:37 +0100

    inclusions to primitives

--- src/imagine/utils/primitives.hpp
+++ src/imagine/utils/primitives.hpp
@@ -5,8 +5,13 @@ namespace Imagine {
     template<typename T>
     struct Point{
       Point(T x, T y) : coordX(x), coordY(y){}
-      T coordX;
-      T coordY;
+      T coordX, coordY;
+    };
+
+    template<typename T>
+    struct Line{
+      Line(Point<T> p1, Point<T> p2) : p1(p1), p2(p2){};
+      Point<T> p1, p2;
     };
 
     template<typename T>
@@ -14,6 +19,13 @@ namespace Imagine {
       Pixel(Point<int> p, T intensity) : point(p), intensity(intensity){}
       Point<int> point;
       T intensity;
+    };
+
+    struct RoiRange{
+      RoiRange(int xMin, int xMax, int yMin, int yMax) :
+        xMin(xMin), xMax(xMax), yMin(yMin), yMax(yMax){}
+
+      int xMin, xMax, yMin, yMax;
     };
   }
 }
