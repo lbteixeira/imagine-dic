82c5d47237f4d01eaa61ab0fee79dac75d129789
Lucas Teixeira <lucas@lbteixeira.com>
2022-04-19 20:13:41 +0200

    new tests based on GNU Octave interp2 function

--- tests/test-interpolator.cpp
+++ tests/test-interpolator.cpp
@@ -1,44 +1,49 @@
 #include <catch2/catch.hpp>
 #include <memory>
-#include <tuple>
 #include "interpolation.hpp"
 
 using namespace Imagine;
 
-TEST_CASE("Interpolation"){
+// The expected results for these tests were taken from GNU Octave's interp2
+// function.
 
-  auto abs_tolerance = 0.01;
+TEST_CASE("Bilinear interpolation"){
 
+  double abs_tolerance = 0.01;
   BilinearInterpolator interpolator(500, 500);
 
-  SECTION("Point 1"){
-    Point<double> p1(0.5, 0.2);
-
+  SECTION("Points in the unit square"){
     std::vector<std::array<int,3>>
-    neighborPx = {{0, 0, 91}, {1, 0, 210}, {1, 1, 95}, {0, 1, 162}};
+    neighborPx = {{0, 0, 68}, {1, 0, 71}, {0, 1, 209}, {1, 1, 180}};
 
-    auto result = interpolator.interpolate(p1, neighborPx);
-    auto expected = 146.1;
+    SECTION("Point 1"){
+      Point<double> p1(0.5, 0.2);
+      double result = interpolator.interpolate(p1, neighborPx);
+      double expected = 94.5;
+      REQUIRE(std::abs(result - expected) < abs_tolerance);
+    };
 
-    REQUIRE(std::abs(result - expected) < abs_tolerance);
+    SECTION("Point 2"){
+      Point<double> p1(0.2, 0.5);
+      double result = interpolator.interpolate(p1, neighborPx);
+      double expected = 135.9;
+      REQUIRE(std::abs(result - expected) < abs_tolerance);
+    };
+
+    SECTION("Point 3 - Located exactly at the upper left corner"){
+      Point<double> p1(0, 0);
+      double result = interpolator.interpolate(p1, neighborPx);
+      double expected = 68;
+      REQUIRE(std::abs(result - expected) < abs_tolerance);
+    };
+
+    SECTION("Point 4 - Located exactly at the lower right corner"){
+      Point<double> p1(1, 1);
+      double result = interpolator.interpolate(p1, neighborPx);
+      double expected = 180;
+      REQUIRE(std::abs(result - expected) < abs_tolerance);
+    };
   };
 
-  SECTION("Point 2"){
-    Point<double> p1(123.37, 157.81);
-    Point<double> p2(123.98, 157.41);
-
-    std::vector<std::array<int,3>>
-    neighborPx = {{123, 157, 97}, {124, 157, 12}, {124, 158, 213}, {123, 158, 255}};
-
-    auto result = interpolator.interpolate(p1, neighborPx);
-    auto expected = 206.42;
-
-    REQUIRE(std::abs(result - expected) < abs_tolerance);
-
-    result = interpolator.interpolate(p2, neighborPx);
-    expected = 95.76;
-
-    REQUIRE(std::abs(result - expected) < abs_tolerance);
-  };
 }
 
